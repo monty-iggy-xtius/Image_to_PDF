e1a12c5ba74ad70801cfa93a6f9fc5a9278a573d
ignatius monty-xtius <monmihalo2324@gmail.com>
2024-08-30 14:54:33 +0300

    Resize images efficiently to preserve quality in PDF format

--- README.md
+++ README.md
@@ -1,16 +1,44 @@
 # Python Image to PDF Converter
 
-Convert your images to PDF
+This Python script takes care of all your Image to PDF needs. It easily converts images in a selected folder to a PDF file while maintaining image quality.
+
+## Features
+
+This program currently has support for PNG and JPG image formats
+
+## Installation
+
+Follow these steps to setup this PDF converter on your system.
+
+### Prerequisites
+
+Before you begin, ensure you have the following dependencies and tools installed:
+
+- Python3
+
+- Pip / Pip3
+
+### Setup and Usage
+
+1. After successfuly cloning this repo, change your working directory to the cloned repository:
+
+```bash
+cd Image_to_PDF
+```
+
+2. Install the project dependencies defined in the requirements.txt file using `pip3 install -r requirements.txt`
+
+3. After all is set, call the main python file from the terminal / cmd using `python3 image2PDF.py`.
+
+4. Type in the folder path to your images and the desired output file name in the prompt that appears and you're all set
+
+
 
 ## Author name
-[ignatius](https://github.com/monty-xtius/Image_to_PDF.git)
-
-## Project Description
-This is a simple python script to convert your images into a pdf file for free!
-
-## python version
-3.10
+[ignatius](https://github.com/monty-iggy-xtius/Image_to_PDF.git)
 
 ## Live link
 ## Contact information
 Email-(monmihalo2324@gmail.com)
+
+If you encounter any issues or have suggestions for improvements, please don't hesitate to [create an issue](https://github.com/monty-iggy-xtius/Image_to_PDF/issues)
--- image2PDF.py
+++ image2PDF.py
@@ -3,6 +3,8 @@ from PIL import Image
 import sys
 import time
 
+DEV = "monmihalo2324@gmail.com"
+
 def get_input():
 	logo = """
 
@@ -15,22 +17,26 @@ Easily convert your images to pdf for free!
 	"""
 	sys.stdout.write(logo)
 	given_path = input("\r[+] Enter path to images: ").strip()
-	destination_file = input("[+] Enter destination file: ").strip()
-	
+	destination_file = input("[+] Enter output file: ").strip()
+
 	try:
-		#change the path to the given directory
+		# change the path to the given directory
 		os.chdir(given_path)
 		# check if provided destination ends with pdf extension
-		if len(destination_file) > 0 and destination_file[:-4] != ".pdf":
-			destination_file = destination_file + ".pdf"
+		if len(destination_file) > 0:
+			destination_file = destination_file + ".pdf" if destination_file.split(".")[-1] != "pdf" else destination_file
 		else:
 			# if no destination is provided use a default one
 			# get current time and replace spaces and colons with underscores
 			current_time = time.ctime(time.time()).replace(":", " ")
 			destination_file = "data_" + "_".join(current_time.split()) + ".pdf"
-			print("  Changed destination file to {}".format(destination_file))
-			
+			print("  Changed output file to {}".format(destination_file))
+
 		convert_files(destination=destination_file)
+
+	except KeyboardInterrupt:
+		print("[-] Quitting Application as per user request")
+		sys.exit()
 
 	except FileNotFoundError:
 		print("[-] Unable to change directory, please try again!")
@@ -42,21 +48,34 @@ Easily convert your images to pdf for free!
 		print("[-] An error occurred, please try again!")
 		get_input()
 
+
 def convert_files(destination):
-	#grab all images in the directory with png or jpg extensions
-	image_files = [file for file in os.listdir() if file.endswith(".jpg") or file.endswith(".png")]
+	# grab all images in the directory with png or jpg extensions
+	image_files = [file for file in os.listdir() if file.endswith(
+	    ".jpg") or file.endswith(".png")]
+
+	# sort the images based on names
+	image_files.sort()
 
 	if len(image_files) > 0:
 		try:
-			#open the images and convert them to RGB format
+			# open the images and convert them to RGB format
 			opened_images = [Image.open(file).convert("RGB") for file in image_files]
 
-			#save images to pdf format
-			#start from the 2nd image to avoid saving the first image twice
-			opened_images[0].save(destination, save_all=True, append_images=opened_images[1:])
+			# get the sizes of the images and find the largest
+			final_width, final_height = max([image_file.size for image_file in opened_images])
+
+			# resizing the images based on largest image size
+			resized_images = [image_file.resize((final_width, final_height), resample=Image.LANCZOS) for image_file in opened_images]
+
+			# save images to pdf format
+			# start from the 2nd image to avoid saving the first image twice
+			resized_images[0].save(destination, save_all=True, append_images=resized_images[1:])
 			
 		except Exception as e:
-			print(e)
+			# print(e)
+			print(f"[-] Something went wrong in the application! Please contact {DEV}")
+			pass
 		print("  Done! Th@nk you for using i3 :)")
 	else:
 		print("[-] No images detected in directory! ")
